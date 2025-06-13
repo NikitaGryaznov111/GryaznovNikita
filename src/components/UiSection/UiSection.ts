8f693d86785ee51967951189415859c5d2c89adf
Nikita <nikitagryaznov1997@gmail.com>
2025-06-13 17:31:24 +0700

    feat: Создал компонент секции полей ввода

--- src/components/UiSection/UiSection.ts
+++ src/components/UiSection/UiSection.ts
@@ -3,7 +3,7 @@ export default function renderUiSection(): void {
   const typography = document.getElementById('typography')
   if (!typography) return
   const uiSection = `
-      <section class="${styles.ui}">
+      <section class="${styles.ui}" id="ui">
         <h2 class="${styles.uiTitle}">UI</h2>
         <h3 class="${styles.uiSubtitle}">Текстовое поле</h3>
         <div class="${styles.uiInputs}">
@@ -12,24 +12,71 @@ export default function renderUiSection(): void {
           </div>
 
           <div class="${styles.uiInputContainer}">
-            <p class="${styles.hoverText}">Ховер</p>
-            <input class="${styles.hoverState}" type="text" id="email-hover" placeholder="E-mail">
+            <p class="${styles.hoverText}" >Ховер</p>
+            <input class="${styles.hoverState}"  type="text" placeholder="E-mail">
           </div>
 
-          <div class="${styles.uiInputContainer}">
-            <p class="${styles.inputText}">Ввод</p>
-            <p class="${styles.inputTextEmail}">E-mail</p>
-            <input class="${styles.inputState}" type="text" id="email-input" placeholder="office@make.st">
+          <div class="${styles.uiInputContainer}" >
+            <p class="${styles.inputText}" id="inputText">Ввод</p>
+            <p class="${styles.inputTextEmail}" id="inputTextEmail">E-mail</p>
+            <input  type="text" id="inputState" placeholder="office@make.st">
           </div>
 
-          <div class="${styles.uiInputContainer}">
-            <p class="${styles.errorText}">Ошибка</p>
-            <p>E-mail</p>
-            <input class="${styles.errorState}" type="text" id="email-error" placeholder="officemake.st">
-            <p class="${styles.uiInputErrorMessage}">Текст ошибки</p>
+          <div class="${styles.uiInputContainer}" >
+            <p class="${styles.errorText}" id="errorText">Ошибка</p>
+            <p class="${styles.errorTextEmail}" id="errorTextEmail">E-mail</p>
+            <input type="text" id="errorState" placeholder="officemake.st">
+            <p class="${styles.errorMessage}" id="errorMessage">Текст ошибки</p>
           </div>
         </div>
       </section>
     `
   typography.insertAdjacentHTML('afterend', uiSection)
+
+  // Добавление стилей при вводе данных
+  const inputState = document.getElementById('inputState') as HTMLInputElement
+  const inputText = document.getElementById('inputText')
+  const inputTextEmail = document.getElementById('inputTextEmail')
+
+  function updateInputState(): void {
+    const value = inputState.value.trim()
+
+    if (value !== '') {
+      inputState.classList.add(styles.inputState)
+      inputText!.style.opacity = '1'
+      inputTextEmail!.style.opacity = '1'
+    } else {
+      inputState.classList.remove(styles.inputState)
+      inputText!.style.opacity = '0'
+      inputTextEmail!.style.opacity = '0'
+    }
+  }
+  inputState?.addEventListener('input', updateInputState)
+
+  // Добавление стилей при ошибке
+  const errorState = document.getElementById('errorState') as HTMLInputElement
+  const errorText = document.getElementById('errorText')
+  const errorTextEmail = document.getElementById('errorTextEmail')
+  const errorMessage = document.getElementById('errorMessage')
+
+  function isValidEmail(email: string): boolean {
+    const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return reg.test(String(email).toLocaleLowerCase())
+  } //проверка валидности email
+
+  function errorInputState(): void {
+    const value = errorState.value.trim()
+    if (value !== '' && !isValidEmail(value)) {
+      errorState.classList.add(styles.errorState)
+      errorText!.style.opacity = '1'
+      errorTextEmail!.style.opacity = '1'
+      errorMessage!.style.display = 'block'
+    } else {
+      errorState.classList.remove(styles.errorState)
+      errorText!.style.opacity = '0'
+      errorTextEmail!.style.opacity = '0'
+      errorMessage!.style.display = 'none'
+    }
+  }
+  errorState?.addEventListener('input', errorInputState)
 }
