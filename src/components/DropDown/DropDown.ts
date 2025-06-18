336b704688e05a2f539a1bda87e88c81e9ecabc4
Nikita <nikitagryaznov1997@gmail.com>
2025-06-18 20:57:56 +0700

    feat: Реализовал компонент выпадающего списка

--- src/components/DropDown/DropDown.ts
+++ src/components/DropDown/DropDown.ts
@@ -3,25 +3,34 @@ export default function renderDropDown(): void {
   const uiSection = document.getElementById('ui')
   if (!uiSection) return
   const dropDown = `
-      <div id="dropDown">
+      <div class="${styles.dropdown} id="dropDown">
         <h3 class="${styles.dropdownTitle}">Выпадающий список</h3>
         <p class="${styles.dropdownDescription}">Компонент состоит из текстового поля и выпадающей подсказки</p>
         <div class="${styles.dropdownGrid}">
-          <div class="${styles.dropdownContainer}">
-            <button class="${styles.dropdownBtnDefault}">Выберите что-нибудь</button>
-            <span class="${styles.arrow}"></span>
+          <div class="${styles.dropdownContainerList}">
+            <p class="${styles.dropdownLabel}" id="dropdownLabel">Выберите что-нибудь</p>
+            <div class="${styles.dropdownContainer} ${styles.dropdownContainerHover}" id="dropdownContainerBtn">    
+              <button class="${styles.dropdownBtnSelect}" id="dropdownBtnSelect">Выбранный пункт</button>
+              <span class="${styles.arrow}" id="arrow"></span>
+            </div>
+            <ul class="${styles.dropdownList}" id="dropdownList">
+              <li data-value="first-chair">Первый стул</li>
+              <li data-value="second-chair">Второй стул</li>
+              <li data-value="selected-item">Выбранный пункт</li>
+              <li data-value="escobar-theorem">Теорема Эскобара</li>
+            </ul>
           </div>
-          <div class="${styles.dropdownContainer} ${styles.dropdownContainerHover}">
+          <div class="${styles.dropdownContainer} ${styles.dropdownContainerHoverDefault}">
             <button class="${styles.dropdownBtnHover}">Выберите что-нибудь</button>
             <span class="${styles.arrow}"></span>
           </div>
-          <div class="${styles.dropdownContainerList}">
-            <p class="${styles.dropdownLabel}">Выберите что-нибудь</p>
+          <div class="${styles.dropdownContainerListDefault}">
+            <p class="${styles.dropdownLabelDefault}">Выберите что-нибудь</p>
             <div class="${styles.dropdownContainer}">    
-              <button class="${styles.dropdownBtnSelect}">Выбранный пункт</button>
+              <button>Выбранный пункт</button>
               <span class="${styles.arrow}"></span>
             </div>
-            <ul class="${styles.dropdownList}">
+            <ul class="${styles.dropdownListDefault}">
               <li data-value="first-chair">Первый стул</li>
               <li data-value="second-chair">Второй стул</li>
               <li data-value="selected-item">Выбранный пункт</li>
@@ -32,6 +41,53 @@ export default function renderDropDown(): void {
        </div>
      </div>
   `
-
   uiSection.insertAdjacentHTML('beforeend', dropDown)
+
+  const dropdownContainerBtn = document.getElementById('dropdownContainerBtn')
+  const dropdownList = document.getElementById('dropdownList')
+  const dropdownLabel = document.getElementById('dropdownLabel')
+  const dropdownBtnSelect = document.getElementById('dropdownBtnSelect')
+  const arrow = document.getElementById('arrow')
+
+  if (
+    dropdownList &&
+    dropdownContainerBtn &&
+    dropdownLabel &&
+    dropdownBtnSelect &&
+    arrow
+  ) {
+    const items = Array.from(dropdownList.querySelectorAll('li'))
+    const handlerDropdownContainerBtn = (): void => {
+      if (dropdownList.style.display === 'block') {
+        dropdownList.style.display = 'none'
+        dropdownLabel.style.display = 'none'
+        arrow.style.transform = 'translateY(calc(-50% - 4px)) rotate(135deg)'
+      } else {
+        dropdownList.style.display = 'block'
+        dropdownLabel.style.display = 'block'
+        arrow.style.transform = 'translateY(-4px) rotate(315deg)'
+      }
+    }
+    const handlerItem = (item: HTMLLIElement): void => {
+      dropdownBtnSelect.textContent = item.textContent
+      items.map((li) => li.classList.remove(styles.activeItem))
+      item.classList.add(styles.activeItem)
+    }
+    items.map((item) => {
+      item.addEventListener('click', () => handlerItem(item))
+    })
+    dropdownContainerBtn.addEventListener('click', handlerDropdownContainerBtn)
+    document.addEventListener('click', (event) => {
+      if (event.target instanceof Node) {
+        if (
+          !dropdownContainerBtn.contains(event.target) &&
+          !dropdownList.contains(event.target)
+        ) {
+          dropdownList.style.display = 'none'
+          dropdownLabel.style.display = 'none'
+          arrow.style.transform = 'translateY(calc(-50% - 4px)) rotate(135deg)'
+        }
+      }
+    })
+  }
 }
--- src/components/TextField/TextField.ts
+++ src/components/TextField/TextField.ts
@@ -4,75 +4,59 @@ export default function renderTextField(): void {
   const uiSection = document.getElementById('ui')
   if (!uiSection) return
   const textField = `
-    <div id="textField">
+    <div class="${styles.textField}">
      <h3 class="${styles.textFieldTitle}">Текстовое поле</h3>
         <div class="${styles.textFieldGrid}">
           <div class="${styles.textFieldItem}">
-            <input type="text"  placeholder="E-mail">
+            <p class="${styles.textEmail}" id="textEmail">E-mail</p>
+            <input type="text"  placeholder="E-mail" id="interactiveInput" class="${styles.interactiveInput}">
+            <p class="${styles.errorMessage}" id="errorMessage">Текст ошибки</p>
           </div>
 
           <div class="${styles.textFieldItem}">
-            <p class="${styles.hoverText}" >Ховер</p>
-            <input class="${styles.hoverState}"  type="text" placeholder="E-mail">
+            <input class="${styles.hoverState}" type="text" placeholder="E-mail">
           </div>
 
           <div class="${styles.textFieldItem}" >
-            <p class="${styles.inputText}" id="inputText">Ввод</p>
-            <p class="${styles.inputTextEmail}" id="inputTextEmail">E-mail</p>
-            <input type="text" id="inputState" placeholder="office@make.st">
+            <p class="${styles.textEmailDefault}">E-mail</p>
+            <input class="${styles.inputState}" type="text" placeholder="office@make.st">
           </div>
 
           <div class="${styles.textFieldItem}" >
-            <p class="${styles.errorText}" id="errorText">Ошибка</p>
-            <p class="${styles.errorTextEmail}" id="errorTextEmail">E-mail</p>
-            <input type="text" id="errorState" placeholder="officemake.st">
-            <p class="${styles.errorMessage}" id="errorMessage">Текст ошибки</p>
+            <p class="${styles.errorTextEmail}">E-mail</p>
+            <input class="${styles.errorState}" type="text" id="errorState" placeholder="officemake.st">
+            <p class="${styles.errorMessageDefault}">Текст ошибки</p>
           </div>
         </div>
     </div>
     `
   uiSection.insertAdjacentHTML('beforeend', textField)
-  //Обработка при вводе данных в инпут
-  const inputState = document.getElementById('inputState') as HTMLInputElement
-  const inputText = document.getElementById('inputText')
-  const inputTextEmail = document.getElementById('inputTextEmail')
 
-  function updateInputState(): void {
-    const value = inputState.value.trim()
-
-    if (value !== '') {
-      inputState.classList.add(styles.inputState)
-      inputText!.style.opacity = '1'
-      inputTextEmail!.style.opacity = '1'
-    } else {
-      inputState.classList.remove(styles.inputState)
-      inputText!.style.opacity = '0'
-      inputTextEmail!.style.opacity = '0'
-    }
-  }
-  inputState.removeEventListener('input', updateInputState)
-  inputState.addEventListener('input', updateInputState)
-
-  // Обработка ошибки при вводе данных в инпут
-  const errorState = document.getElementById('errorState') as HTMLInputElement
-  const errorText = document.getElementById('errorText')
-  const errorTextEmail = document.getElementById('errorTextEmail')
+  const input = document.getElementById('interactiveInput') as HTMLInputElement
+  const textEmail = document.getElementById('textEmail')
   const errorMessage = document.getElementById('errorMessage')
 
-  function errorInputState(): void {
-    const value = errorState.value.trim()
+  function handlerInput(): void {
+    const value = input.value.trim()
+
     if (value !== '' && !isValidEmail(value)) {
-      errorState.classList.add(styles.errorState)
-      errorText!.style.opacity = '1'
-      errorTextEmail!.style.opacity = '1'
+      input.classList.remove(styles.inputState)
+      input.classList.add(styles.errorState)
+      textEmail!.style.color = '#e80f3b'
+      textEmail!.style.opacity = '1'
       errorMessage!.style.display = 'block'
     } else {
-      errorState.classList.remove(styles.errorState)
-      errorText!.style.opacity = '0'
-      errorTextEmail!.style.opacity = '0'
+      input.classList.add(styles.inputState)
+      input.classList.remove(styles.errorState)
+      textEmail!.style.color = '#7d7d7d'
+      textEmail!.style.opacity = '1'
       errorMessage!.style.display = 'none'
     }
+    if (value === '') {
+      textEmail!.style.opacity = '0'
+      input.classList.remove(styles.inputState)
+    }
   }
-  errorState.removeEventListener('input', errorInputState)
-  errorState.addEventListener('input', errorInputState)
+  input.removeEventListener('input', handlerInput)
+  input.addEventListener('input', handlerInput)
 }
